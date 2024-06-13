9a9e399a6c2ccfeebf203c26ab286a80777d6f2d
Pradeep Joshi <pradeepjoshi@Pradeeps-MacBook-Pro.local>
2024-06-13 08:23:33 +0530

    initial commit

---
+++ store/readFile.ts
@@ -0,0 +1,36 @@
+"use server";
+import { promises as fs } from "fs";
+
+export default async function loadDataFromFile() {
+  const data = await fs.readFile(process.cwd() + "/store/Trace1.txt", "utf8");
+
+  const lines = data.split("\n");
+  const finalLines: any[] = [];
+
+  lines.forEach((line, i) => {
+    let finalLine = line.trim().replace(/^\s+|\s+$/gm, "");
+    try {
+      if (finalLine.slice(-4) === "}}0}") {
+        finalLine = finalLine.replace("}}0}", "}");
+      } else if (finalLine.slice(-1) === "}") {
+        const regex = /}+$/;
+        finalLine = finalLine.replace(regex, "}");
+      } else {
+        finalLine = finalLine + "}";
+      }
+      const json = JSON.parse(finalLine);
+      finalLines.push(json);
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+    }
+  });
+  return finalLines;
+
+  // fs.writeFile("file.txt", JSON.stringify(finalLines), function (err) {
+  //   if (err) {
+  //     return console.error(err);
+  //   }
+  //   console.log("File created!");
+  // });
+  // console.log(finalLines);
+}
